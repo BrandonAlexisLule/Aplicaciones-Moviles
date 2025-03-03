24937372c9450f8fb3f0f7fd485a262c1c1420d0
BrandonAlexisLule <al19760600@ite.edu.mx>
2025-03-02 23:32:09 -0800

    modificando rutas

--- rutas/lib/screens/home_page.dart
+++ rutas/lib/screens/home_page.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:rutas/pastel.dart';
-import '../localservice.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -10,33 +8,300 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         leading: Icon(Icons.settings, color: Colors.white),
+        actions: [
+          CircleAvatar(
+            backgroundImage: AssetImage('assets/img/logo.jpg'),
+          )
+        ],
         elevation: 2,
         shadowColor: Colors.black,
-        backgroundColor: Colors.lightGreen[400],
+        backgroundColor: Colors.green[200],
         centerTitle: true,
         title: Text('Pastelería La Choys', style: TextStyle( color: Colors.white, fontWeight: FontWeight.bold)),
       ),
-      body: FutureBuilder(
-        future: Localservice().loadPastel(), 
-        builder: (context, snapshot){
-          if(snapshot.hasData){
-            Pastel pastel = snapshot.data as Pastel;
-            return Card( 
-              child: Column(
-                children: [
-                  Text('Pastel: ${pastel.nombre}'),
-                  Text('Descripción: ${pastel.descripcion}'),
-                  Text('Precio: \$${pastel.precio}'),
-                  Image.network(pastel.imagen)
-                ],
-              ),
-            );
-          } else {
-            return Center(
-              child: Text('Data no cargada'),
-            );
-          }
-        }
+      body: ListView(
+        children: [
+          Container(
+            margin: EdgeInsets.all(15),
+            child: Text('Nuestros especiales del mes de marzo', style: TextStyle(fontSize: 30, letterSpacing: 4),),
+          ),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: [
+                Card(
+                  color: Colors.grey[200],
+                  elevation: 10,
+                  margin: EdgeInsets.all(15),
+                  child: Container(
+                    margin: EdgeInsets.all(10),
+                    padding: EdgeInsets.all(10),
+                    child: Column(
+                      children: [
+                        Text('Pastel de chocolate'),
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: Image.asset('assets/img/cake_choco.jpg', fit: BoxFit.cover, width: 200, height: 200),
+                        ),
+                        Row(
+                          children: [
+                            Container(
+                              margin: EdgeInsets.only(top: 20),
+                              child:                             Text('\$243.50 MXN', style: TextStyle(color: Colors.indigo[500], fontWeight: FontWeight.bold, fontSize: 22)),
+                            ),
+                            Container(
+                              padding: EdgeInsets.all(15),
+                              margin: EdgeInsets.only(top: 15, left: 20),
+                              child: ElevatedButton(                              
+                                onPressed: (){}, 
+                                child: Row(
+                                  children: [
+                                    Text('Agregar al carrito', style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold,
+                                      letterSpacing: 2
+                                    )),
+                                    Icon(Icons.shopping_cart, size: 30,)
+                                  ],
+                                )
+                              ),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                Card(
+                  color: Colors.grey[200],
+                  elevation: 10,
+                  margin: EdgeInsets.all(15),
+                  child: Container(
+                    margin: EdgeInsets.all(10),
+                    padding: EdgeInsets.all(10),
+                    child: Column(
+                      children: [
+                        Text('Pastel de chocolate'),
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: Image.asset('assets/img/cake_choco.jpg', fit: BoxFit.cover, width: 200, height: 200),
+                        ),
+                        Row(
+                          children: [
+                            Container(
+                              margin: EdgeInsets.only(top: 20),
+                              child:                             Text('\$243.50 MXN', style: TextStyle(color: Colors.indigo[500], fontWeight: FontWeight.bold, fontSize: 22)),
+                            ),
+                            Container(
+                              padding: EdgeInsets.all(15),
+                              margin: EdgeInsets.only(top: 15, left: 20),
+                              child: ElevatedButton(                              
+                                onPressed: (){}, 
+                                child: Row(
+                                  children: [
+                                    Text('Agregar al carrito', style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold,
+                                      letterSpacing: 2
+                                    )),
+                                    Icon(Icons.shopping_cart, size: 30,)
+                                  ],
+                                )
+                              ),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                Card(
+                  color: Colors.grey[200],
+                  elevation: 10,
+                  margin: EdgeInsets.all(15),
+                  child: Container(
+                    margin: EdgeInsets.all(10),
+                    padding: EdgeInsets.all(10),
+                    child: Column(
+                      children: [
+                        Text('Pastel de chocolate'),
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: Image.asset('assets/img/cake_choco.jpg', fit: BoxFit.cover, width: 200, height: 200),
+                        ),
+                        Row(
+                          children: [
+                            Container(
+                              margin: EdgeInsets.only(top: 20),
+                              child:                             Text('\$243.50 MXN', style: TextStyle(color: Colors.indigo[500], fontWeight: FontWeight.bold, fontSize: 22)),
+                            ),
+                            Container(
+                              padding: EdgeInsets.all(15),
+                              margin: EdgeInsets.only(top: 15, left: 20),
+                              child: ElevatedButton(                              
+                                onPressed: (){}, 
+                                child: Row(
+                                  children: [
+                                    Text('Agregar al carrito', style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold,
+                                      letterSpacing: 2
+                                    )),
+                                    Icon(Icons.shopping_cart, size: 30,)
+                                  ],
+                                )
+                              ),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.all(15),
+            child: Text('Los más buscados', style: TextStyle(fontSize: 30, letterSpacing: 4),),
+          ),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: [
+                Card(
+                  color: Colors.grey[200],
+                  elevation: 10,
+                  margin: EdgeInsets.all(15),
+                  child: Container(
+                    margin: EdgeInsets.all(10),
+                    padding: EdgeInsets.all(10),
+                    child: Column(
+                      children: [
+                        Text('Pastel de chocolate'),
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: Image.asset('assets/img/cake_choco.jpg', fit: BoxFit.cover, width: 200, height: 200),
+                        ),
+                        Row(
+                          children: [
+                            Container(
+                              margin: EdgeInsets.only(top: 20),
+                              child:                             Text('\$243.50 MXN', style: TextStyle(color: Colors.indigo[500], fontWeight: FontWeight.bold, fontSize: 22)),
+                            ),
+                            Container(
+                              padding: EdgeInsets.all(15),
+                              margin: EdgeInsets.only(top: 15, left: 20),
+                              child: ElevatedButton(                              
+                                onPressed: (){}, 
+                                child: Row(
+                                  children: [
+                                    Text('Agregar al carrito', style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold,
+                                      letterSpacing: 2
+                                    )),
+                                    Icon(Icons.shopping_cart, size: 30,)
+                                  ],
+                                )
+                              ),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                Card(
+                  color: Colors.grey[200],
+                  elevation: 10,
+                  margin: EdgeInsets.all(15),
+                  child: Container(
+                    margin: EdgeInsets.all(10),
+                    padding: EdgeInsets.all(10),
+                    child: Column(
+                      children: [
+                        Text('Pastel de chocolate'),
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: Image.asset('assets/img/cake_choco.jpg', fit: BoxFit.cover, width: 200, height: 200),
+                        ),
+                        Row(
+                          children: [
+                            Container(
+                              margin: EdgeInsets.only(top: 20),
+                              child:                             Text('\$243.50 MXN', style: TextStyle(color: Colors.indigo[500], fontWeight: FontWeight.bold, fontSize: 22)),
+                            ),
+                            Container(
+                              padding: EdgeInsets.all(15),
+                              margin: EdgeInsets.only(top: 15, left: 20),
+                              child: ElevatedButton(                              
+                                onPressed: (){}, 
+                                child: Row(
+                                  children: [
+                                    Text('Agregar al carrito', style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold,
+                                      letterSpacing: 2
+                                    )),
+                                    Icon(Icons.shopping_cart, size: 30,)
+                                  ],
+                                )
+                              ),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                Card(
+                  color: Colors.grey[200],
+                  elevation: 10,
+                  margin: EdgeInsets.all(15),
+                  child: Container(
+                    margin: EdgeInsets.all(10),
+                    padding: EdgeInsets.all(10),
+                    child: Column(
+                      children: [
+                        Text('Pastel de chocolate'),
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: Image.asset('assets/img/cake_choco.jpg', fit: BoxFit.cover, width: 200, height: 200),
+                        ),
+                        Row(
+                          children: [
+                            Container(
+                              margin: EdgeInsets.only(top: 20),
+                              child:                             Text('\$243.50 MXN', style: TextStyle(color: Colors.indigo[500], fontWeight: FontWeight.bold, fontSize: 22)),
+                            ),
+                            Container(
+                              padding: EdgeInsets.all(15),
+                              margin: EdgeInsets.only(top: 15, left: 20),
+                              child: ElevatedButton(                              
+                                onPressed: (){}, 
+                                child: Row(
+                                  children: [
+                                    Text('Agregar al carrito', style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold,
+                                      letterSpacing: 2
+                                    )),
+                                    Icon(Icons.shopping_cart, size: 30,)
+                                  ],
+                                )
+                              ),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       )
     );
   }
